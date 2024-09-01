a5fd90c9254aa32c1a7bfefd0b48995c1e49775c
soumia elyakni <selyakni@gmail.com>
2024-09-01 14:59:46 +0100

    installation envirenoment

---
+++ norophiliaplat-client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './Logo.png.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Norophilia
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
